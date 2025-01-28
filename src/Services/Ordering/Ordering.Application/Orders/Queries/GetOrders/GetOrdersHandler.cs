027dbd88e617f2551b5629e72f54e90849a80efb
Olawale Akin-Odanye <olawale.akinodanye@gmail.com>
2025-01-28 22:18:23 +0100

    feat: complete order application layer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Pagination;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
+using Ordering.Application.Dtos;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext _context) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
+        {
+            var pageIndex = request.PaginationRequest.PageIndex;
+            var pageSize = request.PaginationRequest.PageSize;
+
+            var totalCount = await _context.Orders.LongCountAsync(cancellationToken);
+            var orders = await _context.Orders.Include(o => o.OrderItems)
+                .OrderBy(o => o.OrderName.Value)
+                .Skip(pageSize * pageIndex)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersResult(new PaginatedResult<OrderDto>(pageIndex, pageSize, totalCount, orders.ToOrderDtoList()));
+        }
+    }
+}
